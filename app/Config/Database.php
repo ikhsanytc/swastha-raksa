90c60c950a706eb428753c8387293f41bf1278dd
ikhsanytc <ikhsanm181209@gmail.com>
2025-05-17 22:23:01 +0700

    update database

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -25,31 +25,22 @@ class Database extends Config
      * @var array<string, mixed>
      */
     public array $default = [
-        'DSN'          => '',
-        'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
-        'DBDriver'     => 'MySQLi',
-        'DBPrefix'     => '',
-        'pConnect'     => false,
-        'DBDebug'      => true,
-        'charset'      => 'utf8mb4',
-        'DBCollat'     => 'utf8mb4_general_ci',
-        'swapPre'      => '',
-        'encrypt'      => false,
-        'compress'     => false,
-        'strictOn'     => false,
-        'failover'     => [],
-        'port'         => 3306,
-        'numberNative' => false,
-        'foundRows'    => false,
-        'dateFormat'   => [
+        'database'    => WRITEPATH . 'database' . DIRECTORY_SEPARATOR . 'db.sqlite',
+        'DBDriver'    => 'SQLite3',
+        'DBPrefix'    => 'swastha_raksa_',
+        'DBDebug'     => true,
+        'swapPre'     => '',
+        'failover'    => [],
+        'foreignKeys' => true,
+        'busyTimeout' => 1000,
+        'synchronous' => null,
+        'dateFormat'  => [
             'date'     => 'Y-m-d',
             'datetime' => 'Y-m-d H:i:s',
             'time'     => 'H:i:s',
         ],
     ];
+
 
     //    /**
     //     * Sample database connection for SQLite3.
