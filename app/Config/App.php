0e3f39471882d34ff4c05792688bb535233ce2ba
ikhsanytc <ikhsanm181209@gmail.com>
2025-05-17 21:30:40 +0700

    update

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = (isset($_SERVER['HTTP_HOST']) ? 'http://' . $_SERVER['HTTP_HOST'] . '/' : 'http://localhost/');
+    public string $baseURL = 'https://swastha-raksa-production-1a51.up.railway.app/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
